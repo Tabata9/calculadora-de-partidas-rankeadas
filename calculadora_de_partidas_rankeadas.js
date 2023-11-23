6147ae5c33920084d8997d82878717fdc41a5abc
Tabata9 <tabatarsouza@gmail.com>
2023-11-23 17:04:24 -0300

    first commit

---
+++ calculadora_de_partidas_rankeadas.js
@@ -0,0 +1,31 @@
+function calcularNivel(vitorias, derrotas) {
+    // Calcula o saldo de Ranqueadas
+    var saldoVitorias = vitorias - derrotas;
+
+    // Determina o nível com base no saldo de vitórias
+    var nivel;
+    
+    
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    // Exibe a mensagem final
+    console.log("O Herói tem um saldo de " + saldoVitorias + " está no nível de " + nivel);
+}
+
+// Exemplo de uso
+  calcularNivel(125, 35);// Substitua os valores de vitórias e derrotas pelos desejados
